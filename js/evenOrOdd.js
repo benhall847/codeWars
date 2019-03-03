0549b8684ee25b8141755d1ab2597c551b3003e7
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-03 14:57:24 -0500

    added JavaScript challenges

---
+++ js/evenOrOdd.js
@@ -0,0 +1,5 @@
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+even_or_odd = num => num % 2 === 0 ? "Even" : "Odd"
