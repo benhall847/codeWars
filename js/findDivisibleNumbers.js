6c5eb63b66c7f272a1ffdd23ccc157d97dbadc46
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-11 09:00:07 -0400

    modified solution

--- js/findDivisibleNumbers.js
+++ js/findDivisibleNumbers.js
@@ -3,12 +3,9 @@
 // argument is an array of numbers and the second is the divisor.
 
 function divisibleBy(numbers, divisor){
-    function isDivisible(number){
-        return number % divisor === 0
-    }
-    return numbers.filter(isDivisible);
+    return numbers.filter(num => num % divisor === 0);
 }
 
-// let myArray = [1, 2, 3, 4, 5, 6];
+let myArray = [1, 2, 3, 4, 5, 6];
 
-// console.log(divisibleBy(myArray, 2))
+console.log(divisibleBy(myArray, 2))
