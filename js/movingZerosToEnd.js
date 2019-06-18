f7982e39bf3afaba25eec7729bdb2fd68df0f1dc
Benjamin Hallquist <benhall84707@gmail.com>
2019-06-18 17:10:20 -0400

    completed 5 kyu challenge -Moving Zeros To The End-

---
+++ js/movingZerosToEnd.js
@@ -0,0 +1,14 @@
+var moveZeros = function (arr) {
+    let results = []
+    let zeros = []
+
+    arr.forEach(num=>{
+        if(num === 0){
+            zeros.push(num)
+        }else{
+            results.push(num)
+        }
+    })
+    
+    return [...results, ...zeros]
+}
